33cce9ee850377784418ca63863109e1148de5fb
Mikhail Kashkin <xen@users.noreply.github.com>
2016-02-12 14:09:16 +0200

    Update models.py

--- project/auth/models.py
+++ project/auth/models.py
@@ -6,7 +6,7 @@ from flask.ext.login import UserMixin
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
-    # tables name convention is hard topick, usually I'm against plural forms
+    # tables name convention is contradictory topic, usually I'm against plural forms
     # when name tables, but user is reserved word in post databases,
     # so this is only case when it is allowed to use plural in my teams.
     id = db.Column(db.Integer, primary_key=True)
